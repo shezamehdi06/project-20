a3de2f577a72e1ad246df56981c7627abd19e08f
shezamehdi06 <64864204+shezamehdi06@users.noreply.github.com>
2020-06-07 19:36:50 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+var car,wall;
+var speed,weight;
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
